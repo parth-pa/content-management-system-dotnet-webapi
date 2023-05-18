ba6086d03e1244805138f59f04c97895888b0837
Yash Hindocha <yash.hindocha@ispl.com>
2023-05-18 12:22:26 +0530

    restore.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -211,5 +211,31 @@ namespace keyclock_Authentication.Controllers
             conn.Close();
             return Ok(cms);
         }
+
+
+        [HttpPut]
+        [Authorize]
+
+        public ActionResult restore(int id, int id1)
+        {
+            string data = _Configuration.GetConnectionString("conn");
+            NpgsqlConnection conn = new NpgsqlConnection(data);
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand();
+            command.Connection = conn;
+            command.CommandType = CommandType.Text;
+            command.CommandText = $"select * from cms_deletedata_true({id},{id1});";
+            int a = command.ExecuteNonQuery();
+            if (a == 0)
+            {
+                return BadRequest(new { fail = " failed" });
+                conn.Close();
+            }
+            else
+            {
+                return Ok(new { success = "done" });
+                conn.Close();
+            }
+        }
     }
 }
